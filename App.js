4b5bd87f7ccb2761adcf95eb9617899659d0fcc2
divyanshuispathak <divyanshu.pathak1997@gmail.com>
2024-11-05 17:17:56 +0530

    all about jsx and react components

--- App.js
+++ App.js
@@ -1,6 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("root", {}, "Divyanshu Pathak");
-const root = ReactDOM.createRoot(parent);
-root.render(parent);
+// (JSX transpiled to an React.createElement(object) and then it is rendered as an HTMLElement)
+// Parcel gives responsibility to babel to transpile the JSX code.
+
+// React Element
+const jsxHeading = <h1 id="heading">Namaste React using JSX</h1>
+
+// React functional Component
+
+const MyCourses = () => 
+    (
+        <div>
+            <h1>System Design</h1>
+            <h1>Data Structures & Algorithm</h1>
+        </div>
+    )
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<MyCourses/>);
